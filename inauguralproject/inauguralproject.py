37b8dcd6c65ba7d5c6b93575a363c3e66cd40de9
sofiesybergs <fbk627@alumni.ku.dk>
2023-03-20 13:52:32 +0100

    run

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -23,8 +23,6 @@ from scipy import optimize
 import pandas as pd 
 import matplotlib.pyplot as plt
 
-import HouseholdSpecializationModel as model
-hsm = model.HouseholdSpecializationModelClass()
 
 class HouseholdSpecializationModelClass:
 
@@ -43,7 +41,7 @@ class HouseholdSpecializationModelClass:
 
         # c. household production
         par.alpha = 0.5
-        par.sigma = 1.0
+        par.sigma = 1
 
         # d. wages
         par.wM = 1.0
@@ -73,8 +71,12 @@ class HouseholdSpecializationModelClass:
         C = par.wM*LM + par.wF*LF
 
         # b. home production
-        H = HM**(1-par.alpha)*HF**par.alpha
-
+        if par.sigma == 0:
+            H = np.fmin(HM,HF)
+        elif par.sigma == 1:
+            H = (HM+1e-10)**(1-par.alpha)*(HF+1e-10)**par.alpha
+        else:
+            H = ((1-par.alpha+1e-10)*(HM+1e-10)**((par.sigma-1+1e-10)/(par.sigma+1e-10))+(par.alpha+1e-10)*(HF+1e-10)**((par.sigma-1+1e-10)/(par.sigma+1e-10)))**((par.sigma+1e-10)/(par.sigma-1+1e-10))
         # c. total consumption utility
         Q = C**par.omega*H**(1-par.omega)
         utility = np.fmax(Q,1e-8)**(1-par.rho)/(1-par.rho)
@@ -127,13 +129,30 @@ class HouseholdSpecializationModelClass:
 
     def solve(self,do_print=False):
         """ solve model continously """
-
-        pass    
+        obj = lambda x: - self.calc_utility(x[0], x[1], x[2], x[3])    
+        bounds = [(0,24)]*4
+        guess = [5]*4
+        result = optimize.minimize(obj, guess, method='SLSQP',bounds=bounds)
+        opt = SimpleNamespace()
+        opt.LM = result.x[0]
+        opt.HM = result.x[1]
+        opt.LF = result.x[2]
+        opt.HF = result.x[3]
+        
+        return opt
 
     def solve_wF_vec(self,discrete=False):
         """ solve model for vector of female wages """
+        par = self.par
+        sol = self.sol
 
-        pass
+        for n, i in enumerate(par.wF_vec) :
+            par.wF = i
+            out = self.solve()
+            sol.LM_vec[n] = out.LM
+            sol.LF_vec[n] = out.LF
+            sol.HM_vec[n] = out.HM
+            sol.HF_vec[n] = out.HF
 
     def run_regression(self):
         """ run regression """
@@ -142,63 +161,107 @@ class HouseholdSpecializationModelClass:
         sol = self.sol
 
         x = np.log(par.wF_vec)
-        y = np.log(sol.HF_vec/sol.HM_vec)
+        y = np.log(sol.HF_vec/(sol.HM_vec+1e-10))
         A = np.vstack([np.ones(x.size),x]).T
         sol.beta0,sol.beta1 = np.linalg.lstsq(A,y,rcond=None)[0]
     
     def estimate(self,alpha=None,sigma=None):
         """ estimate alpha and sigma """
-
-        pass
+        def objective(x, self):
+            par = self.par
+            sol=self.sol
+            par.alpha = x[0]
+            par.sigma = x[1]
+            self.solve_wF_vec()
+            self.run_regression()
+            return (0.4-sol.beta0)**2+(-0.1-sol.beta1)**2
+        guess = [.1]*2
+        bounds = [(0,1), (0,10)]
+        result = optimize.minimize(objective, guess, args = (self), method = 'SLSQP', bounds=bounds)
     
-#Illustrate how HF/HM changes when varying α ∈ {0.25, 0.50, 0.75} and σ = {0.5, 1.0, 1.5}. 
-#model = HouseholdSpecializationModelClass()
 
-#alphas = [0.25, 0.50, 0.75]
-#sigmas = [0.5, 1.0, 1.5]
+model = HouseholdSpecializationModelClass()
 
-for alpha in alphas:
-    for sigma in sigmas:
-        # Set new alpha and sigma values
+
+
+model.solve_discrete()
+print(model.solve_discrete())
+
+list_alphas = [ 0.25, 0.5, 0.75]
+list_sigmas = [ 0.5, 1.0, 1.5]
+results_ratio = {}
+
+
+#Solving
+for sigma in list_sigmas:
+    model.par.sigma = sigma
+    for alpha in list_alphas:
         model.par.alpha = alpha
-        model.par.sigma = sigma
-
-        # Solve the model
         opt = model.solve_discrete()
+        relative_hours = opt.HF/opt.HM
+        results_ratio[(alpha, sigma)] = opt.HF/opt.HM
 
-        # Calculate HF/HM
-        hf_hm_ratio = opt.HF / opt.HM
+        print(f' Sigma = {model.par.sigma:.2f} Alpha = {model.par.alpha:.2f}    Ratio: {relative_hours:.2f}')
 
-        # Print the results
-        print(f"alpha={alpha}, sigma={sigma}, HF/HM={hf_hm_ratio}")
 
-import numpy as np
-import matplotlib.pyplot as plt
 
-# Define parameter values
-alpha_vals = [0.25, 0.5, 0.75]
-sigma_vals = [0.5, 1.0, 1.5]
-
-# Define a function to calculate HF/HM ratio
-def calc_HF_HM(alpha, sigma, wF):
-    HM = (wF/(2*alpha))**(1/(1-alpha))
-    HF = (wF/(2*(1-alpha)*sigma))**(1/(1-alpha))
-    return HF/HM
-
-# Define range of wF values to plot
-wF_vals = np.linspace(0.8, 1.2, 100)
-
-# Plot the HF/HM ratio for each combination of alpha and sigma
-for alpha in alpha_vals:
-    for sigma in sigma_vals:
-        ratio_vals = [calc_HF_HM(alpha, sigma, wF) for wF in wF_vals]
-        plt.plot(wF_vals, ratio_vals, label=f"alpha={alpha}, sigma={sigma}")
-
-# Add legend, axis labels, and title
-plt.legend()
-plt.xlabel("wF")
-plt.ylabel("HF/HM")
-plt.title("HF/HM ratio for different values of alpha and sigma")
-
-# Show the plot
+#Plotting
+fig, ax = plt.subplots()
+for sigma in list_sigmas:
+    y = [results_ratio[(alpha, sigma)] for alpha in list_alphas]
+    ax.plot(list_alphas, y, label=f"Sigma={sigma}")
+ax.set_xlabel("alpha")
+ax.set_ylabel("HF/HM ratio")
+ax.legend()
 plt.show()
+
+results_workratio = np.empty(5)
+results_wageratio = np.empty(5)
+
+list_wF = [0.8, 0.9, 1.0, 1.1, 1.2]
+
+for i, wF in enumerate(list_wF):
+    model.par.wF = wF
+    opt = model.solve_discrete()
+    results_workratio[i] = np.log(opt.HF/opt.HM)
+    results_wageratio[i] = np.log(model.par.wF/model.par.wM)
+
+
+print(results_workratio)
+print(results_wageratio)
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.plot(results_wageratio, results_workratio)
+ax.set_title('Home work ratio as function of relative wage')
+ax.set_xlabel('log(wF/wM)')
+ax.set_ylabel('log(HF/HM)');
+
+
+# code
+model.par.wF = 1
+model.par.sigma = 1
+model.par.alpha = 0.5
+
+# model.solve()
+results_con_workratio = np.empty(5)
+results_con_wageratio = np.empty(5)
+
+for i, wF in enumerate(list_wF):
+    model.par.wF = wF
+    opt = model.solve()
+    results_con_workratio[i] = np.log(opt.HF/opt.HM)
+    results_con_wageratio[i] = np.log(model.par.wF/model.par.wM)
+    print(f'LM = {opt.LM}, HM = {opt.HM}, LF = {opt.LF}, LH = {opt.HF} ')
+
+print(results_con_workratio)
+print(results_con_wageratio)
+
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.plot(results_con_wageratio, results_con_workratio)
+ax.set_title('Home work ratio as function of relative wage')
+ax.set_xlabel('log(wF/wM)')
+ax.set_ylabel('log(HF/HM)');
+
