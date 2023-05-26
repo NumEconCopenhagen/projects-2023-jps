1a2305506b5c549145e7f5213a3c4031464d5d16
Joakim48 <rxb527@alumni.ku.dk>
2023-05-26 17:36:08 +0200

    alt i py filer

--- examproject/examprojectQ2.py
+++ examproject/examprojectQ2.py
@@ -131,3 +131,93 @@ def calculate_new_profit():
 
 # 2.4 
 
+def calculate_optimal_delta():
+    # Function to calculate ell_t given kappa_t
+    def calculate_ell(kappa):
+        return ((1 - eta) * kappa / w) ** (1 / eta)
+
+    # Number of shock series to average over
+    K = 100
+
+    # Generate shock series
+    np.random.seed(100)
+    shock_series = np.random.normal(-0.5 * sigma_epsilon ** 2, sigma_epsilon, size=(K, T))
+
+    # Range of delta values to search over
+    delta_max = np.linspace(0, 0.2, 50)
+
+    # Calculate ex-post values for each delta value
+    values_prev = np.zeros(len(delta_max))
+
+    for d, delta in enumerate(delta_max):
+        delta_prev = np.zeros(K)
+
+        for k in range(K):
+            kappa_minus_1 = 1.0  # Initial shock
+            ell_minus_1 = 0.0  # Initial employment
+
+            value_prev = 0.0
+
+            for t in range(T):
+                kappa = np.exp(rho * np.log(kappa_minus_1) + shock_series[k, t])
+                ell_prev = calculate_ell(kappa)
+
+                if np.abs(ell_minus_1 - ell_prev) > delta:
+                    ell = ell_prev
+                else:
+                    ell = ell_minus_1
+
+                profit = kappa * ell ** (1 - eta) - w * ell - (ell != ell_minus_1) * iota
+                value_prev += R ** (-t) * profit
+
+                kappa_minus_1 = kappa
+                ell_minus_1 = ell
+
+            delta_prev[k] = value_prev
+
+        values_prev[d] = np.mean(delta_prev)
+
+    # Find optimal delta that maximizes H
+    optimal_delta = delta_max[np.argmax(values_prev)]
+    H_max = np.max(values_prev)
+
+    return optimal_delta, H_max
+
+# 2.5
+
+# We Change the discount rate for future profits 
+R1 = (1 + 0.001)**(1/12)
+def calculate_expected_profit_discount_rate():
+    # New policy function
+    def policy(kappa, ell_prev, Delta):
+        ell_star = ((1 - eta) * kappa / w) ** (1 / eta)
+        if abs(ell_prev - ell_star) > Delta:
+            return ell_star
+        else:
+            return ell_prev
+    
+    # Define the profit function
+    def profit(kappa, ell, ell_prev):
+        return kappa * ell**(1 - eta) - w * ell - (ell != ell_prev) * iota
+    
+    # Creating an array to store the ex values
+    h_values_new = np.zeros(K)
+    
+    # Generate K random shock series and calculate the ex post value for each
+    np.random.seed(100)  # Set a random seed for reproducibility
+    Delta = 0.05  # Adjustment threshold
+    for k in range(K): # For each shock series
+        epsilon = np.random.normal(-0.5 * sigma_epsilon**2, sigma_epsilon, T)
+        kappa = np.exp(rho * np.log(1) + epsilon)  # Assume kappa_{-1} = 1
+        ell_prev = 0  # Assume ell_{-1} = 0
+        for t in range(T): # For each period
+            ell = policy(kappa[t], ell_prev, Delta)
+            h_values_new[k] += R1**(-t) * profit(kappa[t], ell, ell_prev)
+            ell_prev = ell
+    
+    # Calculate the ex ante expected value for the new policy
+    H_new = np.mean(h_values_new)
+    
+    
+    # Return the results
+    return H_new
